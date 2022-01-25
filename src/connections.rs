3cef9e5ffd360d6b8a3d7255cd3e945cb33ba3a5
Denys Vitali <denys@denv.it>
2022-01-25 23:08:10 +0100

    fix: improve JSON parsing

--- src/connections.rs
+++ src/connections.rs
@@ -1,4 +1,4 @@
-use chrono::{Datelike, TimeZone, Utc};
+use chrono::{Utc};
 use simple_error::SimpleError;
 
 use crate::make_request;
@@ -31,86 +31,109 @@ pub async fn get_connections(from: &str, from_type: LocationType, to: &str, to_t
 
     Ok(serde_json::from_str(
         response
-    ).unwrap_or(VerbindungenResults {
-        verbindungen: vec![],
-        earlier_url: None,
-        late_url: None,
-        verbindung_preis_url: "".to_string(),
-    }))
+    ).unwrap())
 }
 
+mod tests {
+    use chrono::{Datelike, TimeZone, Utc};
+    use crate::connections::get_connections;
+    use crate::models::location::LocationType;
 
-#[actix_rt::test]
-pub async fn test_get_connection() {
-    let today = chrono::offset::Local::now();
-    let date = Utc.ymd(today.year(), today.month(), today.day()).and_hms(12, 0, 0);
-    let conn = get_connections("Zürich HB",
-                               LocationType::Station,
-                               "Basel",
-                               LocationType::Station,
-                               &date);
-    let conn_res = conn.await;
-    assert!(conn_res.is_ok());
-    let verbindungen_res = conn_res.unwrap();
-    assert!(verbindungen_res.verbindungen.len() > 0);
-    println!("Connections = {:?}", verbindungen_res);
-}
+    #[actix_rt::test]
+    pub async fn test_get_connection() {
+        let today = chrono::offset::Local::now();
+        let date = chrono::Utc.ymd(today.year(), today.month(), today.day()).and_hms(12, 0, 0);
+        let conn = get_connections("Zürich HB",
+                                   LocationType::Station,
+                                   "Basel",
+                                   LocationType::Station,
+                                   &date);
+        let conn_res = conn.await;
+        assert!(conn_res.is_ok());
+        let verbindungen_res = conn_res.unwrap();
+        assert!(verbindungen_res.verbindungen.len() > 0);
+        println!("Connections = {:?}", verbindungen_res);
+    }
 
-#[actix_rt::test]
-pub async fn test_get_connection2() {
-    let today = chrono::offset::Local::now();
-    let date = Utc.ymd(today.year(), today.month(), today.day()).and_hms(12, 0, 0);
-    let conn = get_connections("Chiasso",
-                               LocationType::Station,
-                               "Zürich HB",
-                               LocationType::Station,
-                               &date);
-    let conn_res = conn.await;
-    assert!(conn_res.is_ok());
-    let verbindungen_res = conn_res.unwrap();
-    assert!(verbindungen_res.verbindungen.len() > 0);
-    println!("Connections = {:?}", verbindungen_res);
-}
+    #[actix_rt::test]
+    pub async fn test_get_connection2() {
+        let today = chrono::offset::Local::now();
+        let date = Utc.ymd(today.year(), today.month(), today.day()).and_hms(12, 0, 0);
+        let conn = get_connections("Chiasso",
+                                   LocationType::Station,
+                                   "Zürich HB",
+                                   LocationType::Station,
+                                   &date);
+        let conn_res = conn.await;
+        assert!(conn_res.is_ok());
+        let verbindungen_res = conn_res.unwrap();
+        assert!(verbindungen_res.verbindungen.len() > 0);
+        println!("Connections = {:?}", verbindungen_res);
+    }
 
-#[actix_rt::test]
-pub async fn test_get_connections3() {
-    let today = chrono::offset::Local::now();
-    let date =
-        Utc.ymd(today.year(), today.month(), today.day())
-            .and_hms(12, 0, 0);
-    let conn = get_connections(
-        "Zürich HB",
-        LocationType::Station,
-        "Dübendorf, Bahnof",
-        LocationType::Station,
-        &date,
-    );
+    #[actix_rt::test]
+    pub async fn test_get_connections3() {
+        let today = chrono::offset::Local::now();
+        let date =
+            Utc.ymd(today.year(), today.month(), today.day())
+                .and_hms(12, 0, 0);
+        let conn = get_connections(
+            "Zürich HB",
+            LocationType::Station,
+            "Dübendorf, Bahnof",
+            LocationType::Station,
+            &date,
+        );
 
-    let conn_res = conn.await;
-    assert!(conn_res.is_ok());
-    let verbindungen_res = conn_res.unwrap();
-    println!("Connections = {}", verbindungen_res)
-}
+        let conn_res = conn.await;
+        assert!(conn_res.is_ok());
+        let verbindungen_res = conn_res.unwrap();
+        println!("Connections = {}", verbindungen_res)
+    }
 
-#[actix_rt::test]
-pub async fn test_get_connections4() {
-    let today = chrono::offset::Local::now();
-    let date =
-        Utc.ymd(today.year(), today.month(), today.day())
-            .and_hms(12, 0, 0);
-    let conn = get_connections("Chiasso",
-                               LocationType::Address,
-                               "Zürich",
-                               LocationType::Address,
-                               &date,
-    );
+    #[actix_rt::test]
+    pub async fn test_get_connections4() {
+        let today = chrono::offset::Local::now();
+        let date =
+            Utc.ymd(today.year(), today.month(), today.day())
+                .and_hms(12, 0, 0);
+        let conn = get_connections("Chiasso",
+                                   LocationType::Address,
+                                   "Zürich",
+                                   LocationType::Address,
+                                   &date,
+        );
 
-    let conn_res = conn.await;
-    assert!(conn_res.is_ok());
-    let verbindungen_res = conn_res.unwrap();
-    println!("Connections = {}", verbindungen_res);
+        let conn_res = conn.await;
+        assert!(conn_res.is_ok());
+        let verbindungen_res = conn_res.unwrap();
+        println!("Connections = {}", verbindungen_res);
 
-    for c in verbindungen_res.verbindungen {
-        println!("Conn: {}, Duration = {:?}", c, c.duration());
+        for c in verbindungen_res.verbindungen {
+            println!("Conn: {}, Duration = {:?}", c, c.duration());
+        }
+    }
+
+    #[actix_rt::test]
+    pub async fn test_get_connections5() {
+        let today = chrono::offset::Local::now();
+        let date =
+            Utc.ymd(today.year(), today.month(), today.day())
+                .and_hms(12, 0, 0);
+        let conn = get_connections("Im Tiergraten, 8055 Zürich, Zürich",
+                                   LocationType::Address,
+                                   "8005 Zürich, Hardturmstrasse 3",
+                                   LocationType::Address,
+                                   &date,
+        );
+
+        let conn_res = conn.await;
+        assert!(conn_res.is_ok());
+        let verbindungen_res = conn_res.unwrap();
+        println!("Connections = {}", verbindungen_res);
+
+        for c in verbindungen_res.verbindungen {
+            println!("Conn: {}, Duration = {:?}", c, c.duration());
+        }
     }
 }
